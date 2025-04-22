93ea7f8f5632ad03cfd29370984f3c2ac0aff6e6
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-22 17:21:43 +0300

    react egitimine giris 01 commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Text from './components/Text';
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        react egitimi 1
+      </div>
+      <div>
+        react egitimi 1
+      </div>
+      <div>
+        react egitimi 1
+      </div>
+      <Text />
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/components/Text.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div>
+      React 1 Egitimine Hoşgeldiniz
+    </div>
+  )
+}
+
+export default Text
