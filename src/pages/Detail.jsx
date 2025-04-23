b2de39ca1f02653c69f3e7692406807b0bd8748f
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-23 22:31:34 +0300

    indexe gore objelerin bilgilerini ilgili sayfaya yazdirma

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,12 +6,22 @@ import { data } from './Home';
 const Detail = () => {
     const {id} = useParams();
 
+    const [getData, setGetData] = useState(null);
+
+    useEffect(() => {
+        if(id){
+            setGetData(data.find((item) => item.id == id))
+        }
+    }, [id])
+
+    console.log(getData, "getData")
     
     
 
   return (
     <div>
-      Detail
+      <div>{getData?.name}</div>
+      <div>{getData?.description}</div>
     </div>
   )
 }
