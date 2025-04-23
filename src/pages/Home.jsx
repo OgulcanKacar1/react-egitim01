63282d3e986d1284fa20bf297eb41f5769c06414
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-23 22:07:50 +0300

    data objesi ile verileri mapleyerek ekrana yazdirma

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+    
+
+    const data = [
+        {
+            id: 0,
+            name:"React",
+            description: "React is a JavaScript library for building user interfaces.",
+        },
+        {
+            id: 1,
+            name:"Vue",
+            description: "Vue is a progressive framework for building user interfaces.",
+        },
+        {
+            id: 2,
+            name:"Angular",
+            description: "Angular is a platform for building mobile and desktop web applications.",
+        }
+    ]
+
   return (
     <div>
-      Home
+      {
+        data.map((item, index) => (
+            <div onClick={} style={{cursor: 'pointer', marginBottom: '30px'}} key={index}>
+                <div>{item.name}</div>
+                <div>{item.description}</div>
+            </div>
+        )
+
+        )
+      }
     </div>
   )
 }
