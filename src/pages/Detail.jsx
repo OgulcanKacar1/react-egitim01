660d87512da4217bd1d70adee10a99dc8f298d14
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-23 22:24:54 +0300

    datadaki indexe gore details/index linkine gitme

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams,} from 'react-router-dom'
+import { data } from './Home';
 
 const Detail = () => {
+    const {id} = useParams();
+
+    
+    
+
   return (
     <div>
       Detail
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const Home = () => {
-    
+export const data = [
+    {
+        id: 0,
+        name:"React",
+        description: "React is a JavaScript library for building user interfaces.",
+    },
+    {
+        id: 1,
+        name:"Vue",
+        description: "Vue is a progressive framework for building user interfaces.",
+    },
+    {
+        id: 2,
+        name:"Angular",
+        description: "Angular is a platform for building mobile and desktop web applications.",
+    }
+]
 
-    const data = [
-        {
-            id: 0,
-            name:"React",
-            description: "React is a JavaScript library for building user interfaces.",
-        },
-        {
-            id: 1,
-            name:"Vue",
-            description: "Vue is a progressive framework for building user interfaces.",
-        },
-        {
-            id: 2,
-            name:"Angular",
-            description: "Angular is a platform for building mobile and desktop web applications.",
-        }
-    ]
+const Home = () => {
+    const navigate = useNavigate();
+
+    
 
   return (
     <div>
       {
         data.map((item, index) => (
-            <div onClick={} style={{cursor: 'pointer', marginBottom: '30px'}} key={index}>
+            <div onClick={() => navigate(`detail/${item.id}`)} style={{cursor: 'pointer', marginBottom: '30px'}} key={index}>
                 <div>{item.name}</div>
                 <div>{item.description}</div>
             </div>
